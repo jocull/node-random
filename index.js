5625872a41371bc3b989ddb2e24581e7050b47d3
James O'Cull <james.ocull@gmail.com>
2023-07-19 22:01:56 -0400

    Rewrote cache to use sqlite

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const express = require('express');
 const _ = require('lodash');
-const randomObjectPool = require('./object-pool');
+const objectPoolPromise = require('./object-pool');
 
 const app = express();
 const port = 3000;
-
-app.get('/', async (req, res) => {
-  res.contentType('application/json').send(_.sample(randomObjectPool));
-});
-
-app.get('/slow', async (req, res) => {
-  await randomDelay();
-  res.contentType('application/json').send(_.sample(randomObjectPool));
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
 
 async function randomDelay() {
   return new Promise((resolve) => {
     setTimeout(resolve, Math.round(Math.random() * 50));
   });
 }
+
+(async function main() {
+  const objectPool = await objectPoolPromise;
+
+  app.get('/', async (req, res) => {
+    res.contentType('application/json').send(_.sample(objectPool));
+  });
+
+  app.get('/slow', async (req, res) => {
+    await randomDelay();
+    res.contentType('application/json').send(_.sample(objectPool));
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+})();
--- object-pool.js
+++ object-pool.js
@@ -1,42 +1,68 @@
-const fs = require('fs');
-const os = require('os');
-const readline = require('readline');
-
 const _ = require('lodash');
-const objectGenerator = require('./object-generator');
-const CACHE_FILENAME = '.cached';
+const knex = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    filename: '.cache.db',
+  },
+  useNullAsDefault: false,
+});
+// process.on('SIGINT', () => knex.destroy());
+// process.on('SIGTERM', () => knex.destroy());
 
-let cached = [];
-if (fs.existsSync(CACHE_FILENAME)) {
-  try {
-    cached = fs.readFileSync(CACHE_FILENAME).toString().split('\n').map(line => JSON.parse(line));
-  } catch (err) {
-    console.error('Failed to read from cache', err);
+const objectGenerator = require('./object-generator');
+
+function generateObjectString() {
+  while (true) {
+    try {
+      const obj = objectGenerator.runOnce();
+      obj.randomDouble = Math.random(); // Attach a known random field worth parsing
+      return JSON.stringify(obj);
+    } catch (err) {
+      // TODO: Kind of gross. Can we fix with by limiting max depth?
+      console.error('Generation error - will skip this one', err);
+    }
   }
 }
 
-if (_.isEmpty(cached)) {
-  console.log(`Generating new random object pool, this may take a bit...`);
-  const randomObjectPool = _.range(0, 1000)
-    .map(i => {
-      while (true) {
-        try {
-          const obj = objectGenerator.runOnce();
-          obj.randomDouble = Math.random(); // Attach a known random field worth parsing
-          const str = JSON.stringify(obj, null, 2);
-          return str;
-        } catch (err) {
-          // TODO: Kind of gross. Can we fix with by limiting max depth?
-          console.error('Generation error - will skip this one', err);
-        }
-      }
-    });
-
-  cached = randomObjectPool;
-  fs.writeFileSync(CACHE_FILENAME, '');
-  randomObjectPool.forEach((obj, idx) => fs.appendFileSync(CACHE_FILENAME, (idx > 0 ? os.EOL : '') + JSON.stringify(obj)));
-} else {
-  console.log(`Using cached object pool`);
+async function createTableIfNotExists() {
+  try {
+    const tableExists = await knex.schema.hasTable('object_cache');
+    if (!tableExists) {
+      await knex.schema.createTable('object_cache', (table) => {
+        table.increments('id').primary();
+        table.string('value');
+      });
+      console.log('Cache table created');
+    } else {
+      console.log('Cache table already exists');
+    }
+    return (await knex.raw(`SELECT COUNT(*) as count FROM object_cache`))[0].count;
+  } catch (err) {
+    console.error('Error creating cache table:', err.message);
+    throw err;
+  }
 }
 
-module.exports = cached;
+module.exports = (async function () {
+  const poolSize = 1000;
+  let existingRows = await createTableIfNotExists();
+  if (existingRows < poolSize) {
+    console.log("Generating + writing random objects...");
+    for (let i = 1; i <= poolSize; i++) {
+      const objStr = generateObjectString();
+      await knex.raw(`INSERT INTO object_cache (value) VALUES (:value)`, { value: objStr });
+      if (i % 250 == 0 && i > 0) {
+        console.log(`${i}...`);
+      }
+    }
+  } else {
+    console.log("Using objects from cache");
+  }
+  const results = (await knex.raw(`SELECT id, value FROM object_cache ORDER BY RANDOM()`))
+    .map(row => JSON.parse(row.value));
+
+  knex.destroy(); // Close it, we're done
+
+  console.log("Random object pool is ready");
+  return results;
+})();
